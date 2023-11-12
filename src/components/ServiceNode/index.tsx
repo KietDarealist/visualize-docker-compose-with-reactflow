85d94226b524b8a4867b79105c45271129bb2fab
KietDarealist <kietmakietna@gmail.com>
2023-11-12 23:28:42 +0700

    done add edit function for service node

--- src/components/ServiceNode/index.tsx
+++ src/components/ServiceNode/index.tsx
@@ -12,26 +12,30 @@ const ServiceNode: React.FC<IServiceNodeProps> = (props) => {
   let serviceKeyName = Object.keys(data)[0];
 
   const onChange = useCallback((evt: any) => {}, []);
-  const { backgroundColor, borderRadius } = useServiceNode();
+  const { backgroundColor, borderRadius, textColor, fontSize, fontWeight } =
+    useServiceNode();
 
   return (
     <>
       <Handle type="target" position={Position.Top} />
       <div
-        style={{ borderRadius: `${borderRadius}px` }}
+        style={{
+          backgroundColor: backgroundColor,
+          borderRadius: `${borderRadius}px`,
+        }}
         className=" border-solid border  align-center min-h-[130px] max-h-
-      [250px] min-w-[200px] max-w-[220px] overflow-hidden px-2 py-4 border-gray-300 bg-gray-100 flex flex-col justify-center"
+      [250px] min-w-[200px] max-w-[220px] overflow-hidden px-2 py-4 border-gray-300  flex flex-col justify-center"
       >
-        <Typography
-          id="modal-desc"
-          textColor="text.black"
-          fontSize="sm"
-          textAlign="center"
+        <h4
+          style={{
+            color: textColor,
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+          }}
+          className={`text-center text-sm break-word`}
         >
           {serviceKeyName}
-        </Typography>
-        <h4 className="text-center text-sm break-words text-gray-600 font-normal">
-          image: {data?.[serviceKeyName]?.image}
+          <br /> image: {data?.[serviceKeyName]?.image}
         </h4>
       </div>
 
